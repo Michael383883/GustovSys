cd769e8a0c04071d3949cf89b3c881e4ac67ae90
Michael Asencio <164639782+Michael383883@users.noreply.github.com>
2025-02-18 05:49:20 -0400

    feat: link del navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,21 +2,28 @@
 import React from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
-import Image from 'next/image';
-
-const adminImage = '/logo.jpg';
 
 const Navbar = () => {
     return (
         <nav className="bg-gradient-to-r from-red-800 to-yellow-600 p-4 shadow-lg fixed w-full z-50">
             <div className="container mx-auto flex justify-between items-center">
-                <h1 className="text-white text-3xl font-extrabold tracking-wide">Gustov</h1>
+                <h1 className="text-white text-3xl font-extrabold tracking-wide">
+                    <Link href="/">Gustov</Link>
+                </h1>
                 <ul className="flex space-x-6">
-                    {['Inicio', 'Menú', 'Ventas', 'Reportes'].map((item, index) => (
+                    {[
+                        { name: 'Inicio', path: '/' },
+                        { name: 'Menú', path: '/menu' },
+                        { name: 'Ventas', path: '/ventas' },
+                        { name: 'Reportes', path: '/reportes' }
+                    ].map((item, index) => (
                         <li key={index}>
-                            <Link href={`/${item.toLowerCase()}`}>
-                                <motion.span whileHover={{ scale: 1.1 }} className="text-white text-lg font-medium hover:text-gray-300 transition duration-300">
-                                    {item}
+                            <Link href={item.path}>
+                                <motion.span
+                                    whileHover={{ scale: 1.1 }}
+                                    className="text-white text-lg font-medium hover:text-gray-300 transition duration-300"
+                                >
+                                    {item.name}
                                 </motion.span>
                             </Link>
                         </li>
@@ -26,5 +33,4 @@ const Navbar = () => {
         </nav>
     );
 };
-
 export default Navbar;
