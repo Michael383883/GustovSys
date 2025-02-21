58c86c5c25a5aa83e4bfeb12900d1da55962f64a
Michael Asencio <164639782+Michael383883@users.noreply.github.com>
2025-02-21 10:54:07 -0400

    fex:Corecion de el seder para poder ya tener datos desde el principio

--- backend/database/seeders/ProductSeeder.php
+++ backend/database/seeders/ProductSeeder.php
@@ -4,31 +4,52 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 
 class ProductSeeder extends Seeder
 {
     public function run()
     {
+        $imagesPath = database_path('seeders/images/');
+
+
+        $destinationPath = public_path('storage/products/');
+
+        if (!File::exists($destinationPath)) {
+            File::makeDirectory($destinationPath, 0777, true, true);
+        }
+
         $products = [
             [
                 'name' => 'Charque',
                 'price' => 12.00,
-                'image' => 'products/charque.png',
+                'image' => 'charque.png',
             ],
             [
                 'name' => 'Pique',
                 'price' => 14.00,
-                'image' => 'products/pique.png',
+                'image' => 'pique.png',
             ],
             [
                 'name' => 'Pailita',
                 'price' => 10.00,
-                'image' => 'products/pailita.png',
+                'image' => 'pailita.png',
             ],
         ];
 
         foreach ($products as $product) {
-            DB::table('products')->insert($product);
+            $source = $imagesPath . $product['image']; 
+            $destination = $destinationPath . $product['image']; 
+
+            if (File::exists($source)) {
+                File::copy($source, $destination);
+            }
+
+            DB::table('products')->insert([
+                'name' => $product['name'],
+                'price' => $product['price'],
+                'image' => 'products/' . $product['image'],
+            ]);
         }
     }
 }
